4f34687270ad3065570bc0eccd6231388f289e47
Oskar Grunning <oskargrunning@gmail.com>
2019-10-07 13:05:36 +0200

    Add leftover files

---
+++ boxes/fp.ts
@@ -0,0 +1,12 @@
+console.clear ()
+
+//  ┏━╸┏━┓   ╺┳╸┏━┓
+//  ┣╸ ┣━┛    ┃ ┗━┓
+//  ╹  ╹      ╹ ┗━┛
+
+const pokemon = require ('oakdex-pokedex/data/pokemon')
+
+const C = require ('crocks')
+const Future = require ('fluture')
+const R = require ('ramda')
+const S = require ('sanctuary')
